05a039e00bc87788b35017c840ce25a327ebdb65
iam-abhishek-yadav <abhrecscs17@gmail.com>
2024-06-16 22:31:19 +0530

    Update: copy button reset

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ const App: React.FC = () => {
 	const copyPassword = useCallback(() => {
 		window.navigator.clipboard.writeText(password);
 		setText("copied");
+		setTimeout(() => {setText("copy")}, 2000)
 	}, [password]);
 
 	const handleCheckboxChange = (
